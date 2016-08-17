f07e8ab5ed3736f542edd528ffc70ed44f859e35
JB <jonathonbohr@gmail.com>
2016-08-17 17:09:14 -0500

    Update chat.c

--- directchat/chat.c
+++ directchat/chat.c
@@ -1,13 +1,13 @@
-#include <stdio.h> //what is stdio?
-#include <stdlib.h> //i don't see any books here
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
-#include <string.h> //never used this one before
-#include <sys/types.h> //this one either
-#include <unistd.h> //pthreads support
-#include <arpa/inet.h> //struct in_addr, net/host conversion hton
-#include <pthread.h> //obvs
-#include <assert.h> //assert error returns
-#include <netdb.h> //ai_flags
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <assert.h>
+#include <netdb.h>
 
 #define NUM_THREADS 3
 #define BUFSIZE 128
@@ -60,10 +60,10 @@ int main(int argc, char *argv[]) {
 	struct sockaddr_in ServAddr, CliAddr;
 	struct thread_info *tinfo;
 	int errcode;	//getaddrinfo
-	int result_code, thread_args[NUM_THREADS]; //from pthread.h
+	int result_code, thread_args[NUM_THREADS]; //pthread.h
 	pthread_t *threads[NUM_THREADS];
 
-	//lookup host server ports
+	//test host lookup
 	memset(&hints, '\0', sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
