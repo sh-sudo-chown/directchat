a6159678088c68ce5fddd65160c6699acbcd9331
sh-sudo-chown <jonathonbohr@gmail.com>
2016-08-18 16:55:09 -0500

    merge and update complete

--- chat.c
+++ chat.c
@@ -10,45 +10,48 @@
 #include <netdb.h>
 
 #define NUM_THREADS 3
-#define BUFSIZE 32
+#define BUFSIZE 4096
 
-//	SYNTAX
-//	./chat remote_ip connect_port listen_port
-//	ex
-//	./chat 127.0.0.1 55 89
+//	SYNTAX !!! - while testing AI_PASSIVE flags are currently in place of IP address at argv[1]
+//	./chat connect_port listen_port remote_ip
+//   e.g.
+//	./chat '55' '89' '127.0.0.1'
 
 
-void 	*client(void* ServAddr){
+void 	*client(void *ServAddr){
+	pthread_detach(pthread_self());
+//	pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+	struct sockaddr_in *incomming = (struct sockaddr_in *)ServAddr;
 	int sockfd = socket(AF_INET,SOCK_STREAM,0);
 	char sendBuffer[BUFSIZE];
-	while(sendBuffer != "quit"){
-		if(connect(sockfd, (struct sockaddr *) &ServAddr, sizeof(ServAddr)) != 0){
-			for(;;){
-				scanf("%s", sendBuffer);
-				send(sockfd, sendBuffer, BUFSIZE, '\0');
-				printf("-%s", sendBuffer);
-			}
-		}
-	}
+	connect(sockfd, (struct sockaddr *) &incomming, sizeof(incomming));
+		do{
+			scanf("%s", sendBuffer);
+			send(sockfd, sendBuffer, BUFSIZE, '\0');
+			printf("-%s", sendBuffer);
+		}while(strcmp(sendBuffer,"quit\n"));
 	close(sockfd);
 	exit(0);
 }
 
-void	*server(void* CliAddr){
+void	*server(void *CliAddr){
+	pthread_detach(pthread_self());
+//	pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+	struct sockaddr_in *incomming = (struct sockaddr_in *)CliAddr;
 	int sockfd = socket(AF_INET,SOCK_STREAM,0);
 	char rcvBuffer[BUFSIZE];
 	ssize_t BytesRecv;
 	int connfd;
-	socklen_t CliLen = sizeof(CliAddr);
-	bind(sockfd, (struct sockaddr *) &CliAddr, sizeof(*CliAddr));
+	socklen_t CliLen = sizeof(incomming);
+	bind(sockfd, (struct sockaddr *) &incomming, sizeof(*incomming));
 	listen(sockfd, 7);
-	if((connfd=accept(sockfd, (struct sockaddr *) &CliAddr, &CliLen)) != 0 ){
-		for (;;){
+	if((connfd=accept(sockfd, (struct sockaddr *) &incomming, &CliLen)) != 0 ){
+		do{
 			BytesRecv = recv(sockfd, rcvBuffer, BUFSIZE, '\0');
 			if (rcvBuffer > 0){
 				printf("%s: ", rcvBuffer);
 			}
-		}
+		}while(strcmp(rcvBuffer,"quit\n"));
 	}
 	close(sockfd);
 	exit(0);
@@ -58,13 +61,15 @@ void	*server(void* CliAddr){
 int main(int argc, char *argv[]) {
 	struct addrinfo hints, *res;
 	struct sockaddr_in ServAddr, CliAddr;
-	struct thread_info *tinfo;
-	int errcode;	//getaddrinfo
-	int result_code, thread_args[NUM_THREADS]; //from pthread.h
-	pthread_t *threads[NUM_THREADS];
+//	int errcode;	//getaddrinfo
+	int result_code, thread_args[NUM_THREADS]; //pthread.h
+//	pthread_t *threads[NUM_THREADS];
+	pthread_t thread1, thread2;
+	pthread_attr_t tattr;
+	pthread_attr_init(&tattr);
 
-	//lookup host server ports
-	memset(&hints, '\0', sizeof(hints));
+	//test host lookup
+/*	memset(&hints, '\0', sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;	//AI_PASSIVE flag sets host to local, netdb.h
@@ -73,24 +78,22 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 	freeaddrinfo(&hints);
-
+*/
 	//configure connection to server
 	memset(&ServAddr, '\0', sizeof(ServAddr));
 	ServAddr.sin_family = AF_INET;
 	ServAddr.sin_addr.s_addr = AI_PASSIVE;	//AI_PASSIVE flag sets host to local
 	ServAddr.sin_port = htons(*argv[2]);
-		//add test for ServAddr
 
 	//configure connection to client
 	memset(&CliAddr, '\0', sizeof(CliAddr));
-	ServAddr.sin_family = AF_INET;
-	ServAddr.sin_addr.s_addr = AI_PASSIVE;	//AI_PASSIVE flag sets host to local
-	ServAddr.sin_port = htons(*argv[1]);
-	
+	CliAddr.sin_family = AF_INET;
+	CliAddr.sin_addr.s_addr = AI_PASSIVE;	//AI_PASSIVE flag sets host to local
+	CliAddr.sin_port = htons(*argv[1]);
+
 	//configure threads for client then server socket
-	result_code = pthread_create(&threads[0], NULL, server, (void *) &ServAddr);
+	result_code = pthread_create(&thread1, &tattr, client, (void *) &ServAddr);
 	assert(0 == result_code);
-	
-	result_code = pthread_create(&threads[1], NULL, client, (void *) &CliAddr);
+	result_code = pthread_create(&thread2, &tattr, server, (void *) &CliAddr);
 	assert(0 == result_code);
 }
